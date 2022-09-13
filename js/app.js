f41e6e8074f8055920de51f9f040caca589bc0ed
DEVendra211 <92670758+DEVendra211@users.noreply.github.com>
2022-09-13 08:52:43 +0530

    make it responsive

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ fetch(url).then(i => i.json()).then(i => {
         
         for (let i in data) {
             
-            document.querySelector(".data").innerHTML+=`<div class="col-4"><div class="card"><img src="${res[i].image}"><h2>${res[i].title}</h2>
+            document.querySelector(".data").innerHTML+=`<div class="col-4 col-t-6 col-p-12"><div class="card"><img src="${res[i].image}"><h2>${res[i].title}</h2>
             <h3>$${res[i].price}</h3><p>${res[i].description}</p></div></div>`;
             
            
